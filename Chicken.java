029701bb09dc42b9547ad9328324731bf3fc53aa
AlexanderAltamirano <you@example.com>
2017-10-25 11:14:59 -0500

    V1.0. The Chicken class

---
+++ Chicken.java
@@ -0,0 +1,40 @@
+public class Chicken
+{
+	//variables y metodos con miniscula
+	//clases con mayusucula
+	//clases upercamelcase
+	//atributos lowwecamelcase
+	//metodos lowercamelcase
+	//variables lowercamelcase
+	
+		private String name;
+		private String color;
+		private int age;
+		
+		public void initializaData (String name, String color, int age)
+		{
+			this.name=name;
+			this.color=color;
+			this.age=age;
+			
+		}
+
+		public void layAnEgg()
+		{
+			System.out.println("The chicken lay an egg");
+		}
+		public void poop()
+		{
+			System.out.println("The Chicken pooped");
+		}
+		public void drink()
+		{
+			System.out.println("The chicken drank");
+		}
+		public void showData()
+		{
+			System.out.println("The name of the chicken is:  " + name + ",Color:  " + color + "The birhday: " + age);
+		}
+
+
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,12 @@ public class HelloWorld
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Jonathan");
+		Chicken chicken;
+		chicken = new Chicken();
+		chicken.initializaData("lucy","coffe",2);
+		chicken.showData();
+		System.out.println("Chao Jonathan");
+		
 	}
 
 }
