39841e9636c5e00d6edc215f5b59b9b5cd9784bd
AlexanderAltamirano <you@example.com>
2017-11-08 11:34:59 -0500

    Arreglo deber

---
+++ Arreglos/src/ec/edu/espe/arreglos/tipos/TestingAnimals.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.arreglos.tipos;
+import ec.edu.espe.arreglos.modelo.Animal;//control +shit+i importar librerias
+
+
+/**
+ *
+ * @author LABS-DECC
+ */
+public class TestingAnimals {
+    
+    public static void main(String[] args) {
+        Animal animal;
+        
+    }
+}
